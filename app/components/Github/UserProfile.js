04636e3228759af8b00a0116eea3bbd66d58a09e
aborozduhin <aleksandr.borozduhin@artezio.com>
2016-04-06 15:32:24 +0300

    user profile and repos rendering

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -8,9 +8,15 @@ var UserProfiles = React.createClass({
     render: function() {
         return (
             <div>
-                UserProfiles <br />
-                Username: {this.props.username} <br />
-                Bio: {JSON.stringify(this.props.bio)}
+                <h3>User Profile</h3>
+                <ul className="list-group">
+                    {this.props.bio.avatar_url && <li className="list-group-item"><img src={this.props.bio.avatar_url} className="img img-responsive" /></li>}
+                    {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+                    {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
+                    {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
+                    {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
+                    {this.props.bio.company && <li className="list-group=item">Company: {this.props.bio.company}</li>}
+                </ul>
             </div>
         )
     }
