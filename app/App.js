ed623907f9d314dd07446a6ce827edd9c6f5316c
aborozduhin <aleksandr.borozduhin@artezio.com>
2016-04-06 16:40:41 +0300

    es6 syntax

--- app/App.js
+++ app/App.js
@@ -1,15 +1,9 @@
-var React = require("react");
-var ReactDOM = require('react-dom');
+import React from "react";
+import ReactDOM from 'react-dom';
+import { browserHistory, Router, Route } from 'react-router'
+import routes from "./config/routes";
 
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-
-var routes = require("./config/routes");
-
-//var createBrowserHistory = require("react-router/node_modules/history/lib/createBrowserHistory");
-//var history = createBrowserHistory();
-
-var history = require("react-router/lib/browserHistory");
-
-ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById("app"))
+ReactDOM.render(
+    <Router history={browserHistory}>{routes}</Router>, 
+    document.getElementById("app")
+);
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,10 @@
-var React = require('react');
-var Main = require('../components/Main');
-var Home = require('../components/Home');
-var Router = require('react-router');
-var Route = Router.Route;
-var Profile = require('../components/Profile');
+import React from 'react';
+import Main from '../components/Main';
+import Home from '../components/Home';
+import { Router, Route } from 'react-router';
+import Profile from '../components/Profile';
 
-module.exports = (
+export default (
     <Route component={Main}>    
         <Route path="profile/:username" component={Profile} />
         <Route path="/" component={Home} />    
